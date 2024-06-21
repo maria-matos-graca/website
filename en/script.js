a312ab91a5ace51d645aebade462a7a9c3eb8925
maria-matos-graca <114141683+maria-matos-graca@users.noreply.github.com>
2024-06-21 09:58:57 +0100

    dar a volta ao problema da cache com javascript

--- en/script.js
+++ en/script.js
@@ -8,6 +8,25 @@ window.loadContent = function(pageName) {
         })
         .catch(error => console.error('Error loading the page: ', error));
 };
+
+function loadScript(url) {
+    var script = document.createElement('script');
+    script.src = url + '?v=' + new Date().getTime();
+    document.head.appendChild(script);
+}
+
+// Load stylesheets with a timestamp to prevent caching
+function loadStylesheet(url) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url + '?v=' + new Date().getTime();
+    document.head.appendChild(link);
+}
+
+// Load the default content when the page loads
 document.addEventListener('DOMContentLoaded', () => {
+    loadScript('script.js');
+    loadStylesheet('styles.css');
     loadContent('about.html');
 });
+
