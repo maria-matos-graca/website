a4ed8c57aee33e3a13858b07fe77e3fa1d55cb23
maria-matos-graca <114141683+maria-matos-graca@users.noreply.github.com>
2024-06-21 10:01:05 +0100

    ainda javascript para cache

--- script.js
+++ script.js
@@ -8,6 +8,23 @@
           })
           .catch(error => console.error('Error loading the page: ', error));
   };
-  document.addEventListener('DOMContentLoaded', () => {
-      loadContent('about.html');
-  });
+  function loadScript(url) {
+    var script = document.createElement('script');
+    script.src = url + '?v=' + new Date().getTime();
+    document.head.appendChild(script);
+}
+
+// Load stylesheets with a timestamp to prevent caching
+function loadStylesheet(url) {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url + '?v=' + new Date().getTime();
+    document.head.appendChild(link);
+}
+
+// Load the default content when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+    loadScript('script.js');
+    loadStylesheet('styles.css');
+    loadContent('about.html');
+});
